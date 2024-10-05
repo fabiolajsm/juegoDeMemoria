5bd1ed2ca689a32c2bc89922ff367871cbc7a150
Fabiola <suarezfabiola17@gmail.com>
2024-10-05 18:29:33 -0300

    estilos home y placeholder login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonCard,
+  IonCardTitle,
+  IonCardHeader,
+  IonCardContent,
+} from '@ionic/angular/standalone';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonCard,
+    IonCardTitle,
+    IonCardHeader,
+    IonCardContent,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private auth: AuthService, private router: Router) {}
+
+  selectLevel(level: string) {
+    console.log(`Nivel seleccionado: ${level}`);
+    // Aquí puedes navegar a la pantalla del juego o realizar otra acción
+  }
+
+  handleLogout() {
+    this.auth.logout();
+    this.router.navigateByUrl('login');
+  }
 }
